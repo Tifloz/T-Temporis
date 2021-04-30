25bb545264c085788bb5b4f08fffd66481426ecc
Florian Louvet <florian.louvet@epitech.eu>
2021-04-30 16:14:47 +0900

    [+] Added version 1.0

---
+++ T-TemporisV/Card.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+
+namespace T_TemporisV
+{
+    public class Card
+    {
+        private String color;
+        private int ID;
+        private int item_id;
+        private String job;
+        private int level;
+        private string name;
+        private string obt;
+
+        private Obtention obtention;
+        private String rarity;
+
+        public Card(String name, string obt, int ID, int item_id, String rarity, String color, String job, int level)
+        {
+            this.name = name;
+            this.obt = obt;
+            this.ID = ID;
+            this.item_id = item_id;
+            this.rarity = rarity;
+            this.color = color;
+            this.job = job;
+            this.level = level;
+        }
+
+        public string Obt
+        {
+            get => obt;
+            set => obt = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Obtention Obtention
+        {
+            get => obtention;
+            set => obtention = value;
+        }
+
+        public int Id
+        {
+            get => ID;
+            set => ID = value;
+        }
+
+        public int ItemId
+        {
+            get => item_id;
+            set => item_id = value;
+        }
+
+        public string Rarity
+        {
+            get => rarity;
+            set => rarity = value;
+        }
+
+        public string Color
+        {
+            get => color;
+            set => color = value;
+        }
+
+        public string Job
+        {
+            get => job;
+            set => job = value;
+        }
+
+        public int Level
+        {
+            get => level;
+            set => level = value;
+        }
+    }
+
+    public class Obtention
+    {
+        private bool isDrop;
+        private bool unknow = false;
+        public bool IsDrop
+        {
+            get => isDrop;
+            set => isDrop = value;
+        }
+        public bool Unknow
+        {
+            get => unknow;
+            set => unknow = value;
+        }
+        public Dictionary<string, string> ItemDetails
+        {
+            get => itemDetails;
+            set => itemDetails = value;
+        }
+
+        private Dictionary<string, string> itemDetails;
+
+        public Obtention(string obt)
+        {
+            if (obt == null)
+            {
+                unknow = true;
+                return;
+            }
+            this.isDrop = obt.Contains("%");
+            itemDetails = new Dictionary<string, string>();
+            var item_list = obt.Replace(", ", ",").Replace(" (", "(").Split(',');
+            foreach (string item in item_list)
+            {
+                string name = item.Split('(')[0];
+
+                string value = item.Split('(')[1].Replace(")", "").Replace("x", "").Replace("%", "").Trim();
+
+                try
+                {
+                    KeyValuePair<string, string> tmpPair = new KeyValuePair<string, string>(name, value);
+                    if (itemDetails.ContainsKey(tmpPair.Key) == false)
+                    {
+                        itemDetails.Add(name, value);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MaterialMessageBox.Show(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ T-TemporisV/TemporisV.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace T_TemporisV
+{
+    public partial class T_TemporisV : MaterialForm
+    {
+        private CardsManager cardsManager = new CardsManager();
+
+        public T_TemporisV()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Red800, Primary.BlueGrey900, Primary.Red500,
+                Accent.Red400, TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 2; i <= 200; i++)
+            {
+                startLevel_CB.Items.Add(i);
+                finishLevel_CB.Items.Add(i);
+            }
+        }
+
+        private void search_B_Click(object sender, EventArgs e)
+        {
+            export_B.Enabled = false;
+            dropCards_LV.Items.Clear();
+            craftCards_LV.Items.Clear();
+            cardsManager.NeededCCards.Clear();
+            cardsManager.NeededDCards.Clear();
+
+            int tmp = int.Parse(finishLevel_CB.Text) - int.Parse(startLevel_CB.Text);
+
+            cardsManager.GetCards(int.Parse(startLevel_CB.Text), int.Parse(finishLevel_CB.Text));
+            foreach (var card in cardsManager.NeededCCards)
+            {
+                ListViewItem newItem = new ListViewItem();
+                newItem.Text = card.Name;
+                craftCards_LV.Items.Add(newItem);
+            }
+            export_B.Enabled = true;
+            foreach (var card in cardsManager.NeededDCards)
+            {
+                ListViewItem newItem = new ListViewItem();
+                newItem.Text = card.Name;
+                dropCards_LV.Items.Add(newItem);
+            }
+        }
+
+        private void export_B_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog= new SaveFileDialog();
+            saveFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (var writer = new StreamWriter(saveFileDialog.OpenFile()))
+                {
+                    foreach (var card in cardsManager.NeededCCards)
+                    {
+                        if (card.Obtention.Unknow == false)
+                        foreach (var pair in card.Obtention.ItemDetails)
+                        {
+                            writer.WriteLine("{0};{1};", pair.Key, pair.Value);
+                        }
+                    }
+                }
+               
+            }
+        }
+    }
+}
